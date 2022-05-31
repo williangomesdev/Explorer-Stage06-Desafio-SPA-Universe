d20178f55fc24b0da22d6da243bd53bc347813c7
unknown <willianamaroti@gmail.com>
2022-05-31 03:09:31 -0300

    finalização da estilização ajustar alguns recursos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 const app = document.querySelector("#app");
+const body = document.querySelector("body");
+const homeLink = document.querySelector("#homeLink");
+const universeLink = document.querySelector("#universeLink");
+const explorerLink = document.querySelector("#explorerLink");
 
+homeLink.addEventListener("click", () => {
+  homeLink.classList.toggle("active");
+  universeLink.classList.remove("active");
+  explorerLink.classList.remove("active");
+  body.classList.add("home");
+  body.classList.remove("universe");
+  body.classList.remove("explorer");
+});
+
+universeLink.addEventListener("click", () => {
+  universeLink.classList.toggle("active");
+  homeLink.classList.remove("active");
+  explorerLink.classList.remove("active");
+  body.classList.add("universe");
+  body.classList.remove("home");
+  body.classList.remove("explorer");
+});
+
+explorerLink.addEventListener("click", () => {
+  explorerLink.classList.toggle("active");
+  homeLink.classList.remove("active");
+  universeLink.classList.remove("active");
+  body.classList.add("explorer");
+  body.classList.remove("universe");
+  body.classList.remove("home");
+});
+
+//rotas
 //retirar padrão dos links
 function route(event) {
   event = event || window.event;
